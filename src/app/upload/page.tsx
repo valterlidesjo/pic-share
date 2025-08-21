fa3b665b314f72fce6863d2d669307f9cf90d209
valterlidesjo <valle.lidesjo@gmail.com>
2025-08-21 12:32:45 +0200

    eslint

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { useImageUpload } from "@/hooks/useImageUpload";
 import Image from "next/image";
